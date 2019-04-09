f0a69bc5ff5b2734b178dd3ac2069214225e31fe
sguittap <sguittap@gmail.com>
2019-04-09 17:25:55 -0400

    user registration and postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 
 //GET route: /api/users/test
 router.get('/test', (req,res) => res.json({msg:'Users works'}));
+
+//POST route
+router.post('/register', (req, res) => {
+    User.findOne({email:req.body.email})
+        .then(user => {
+            if(user){
+                return res.status(400).json({email:'Email already exists'})
+            } else{
+                const avatar = gravatar.url(req.body.email,{
+                    s:'200', r:'pg', d:'mm'
+                });
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar
+                })
+                bcrypt.genSalt(10, (err, salt) =>{
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
@@ -8,6 +9,10 @@ const profile = require('./routes/api/profile');
 require('dotenv').config();
 
 const app = express();
+
+//BodyParser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
