08acb5d775b224902d7554accaa1a064c3283f01
sguittap <sguittap@gmail.com>
2019-04-09 14:40:36 -0400

    connecting mongoose to db

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-svlrg.mongodb.net/test?retryWrites=true`
+};
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+//Connecting to DB
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(`Error connecting --> ${err}`));
 
 app.get('/', (req, res) => res.send('hello, this is working'));
 
