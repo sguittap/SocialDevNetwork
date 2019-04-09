86837986d4dff0b901bf7dc1fdcabfbad69d03c6
sguittap <sguittap@gmail.com>
2019-04-09 17:45:17 -0400

    added login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcryptjs');
 //GET route: /api/users/test
 router.get('/test', (req,res) => res.json({msg:'Users works'}));
 
-//POST route
+//Register newUser
 router.post('/register', (req, res) => {
     User.findOne({email:req.body.email})
         .then(user => {
@@ -34,6 +34,26 @@ router.post('/register', (req, res) => {
                     })
                 })
             }
+        })
+});
+
+//Login User
+router.post('/login', (req, res) => {
+    const email = req.body.email;    
+    const password = req.body.password;    
+    User.findOne({email})
+        .then(user => {
+            if(!user){
+                return res.status(404).json({email:'User not found'})
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        res.json({msg:'Success'})
+                    } else {
+                        return res.status(400).json({password:'Password incorrect'})
+                    }
+                })
         })
 });
 
