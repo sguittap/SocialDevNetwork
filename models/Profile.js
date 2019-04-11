8186dd5169082f32b9d7ed70d33b31f3b87773af
sguittap <sguittap@gmail.com>
2019-04-11 14:50:08 -0400

    added current user profile route

--- models/Profile.js
+++ models/Profile.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //create
-const ProfileSchema = new Schema({
+const profileSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'users'},
     handle: {type: String, required: true, max: 40},
     company: {type: String},
     website: {type: String},
     location: {type: String},
     status: {type: String, required: true},
-    skills: [String], required: true,
+    skills: {type:[String], required: true},
     bio: {type: String},
     githubusername: {type: String},
     experience:[{
@@ -40,4 +40,4 @@ const ProfileSchema = new Schema({
     }
 });
 
-module.exports = Profile = mongoose.model('profile', ProfileSchema)
+module.exports = Profile = mongoose.model('profile', profileSchema)
