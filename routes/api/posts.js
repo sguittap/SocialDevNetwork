185fdd68f1d27e56eda55392b4e62e50da004fbf
sguittap <sguittap@gmail.com>
2019-04-15 17:32:42 -0400

    delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,10 +4,26 @@ const mongoose = require('mongoose');
 const passport  = require('passport');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 const validatePostInput = require('../../validation/post');
 
 //GET route: /api/posts/test
 router.get('/test', (req,res) => res.json({msg:'Posts works'}));
+
+//Get all posts. route: api/posts
+router.get('/', (req,res) => {
+    Post.find()
+        .sort({date: -1})
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({notpostsfound: 'No posts found'}))
+});
+
+//Get a single post. route: api/posts/:id:
+router.get('/:id', (req,res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({notpostfound: 'No post found with that ID'}))
+});
 
 //POST route: /api/posts
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
@@ -22,6 +38,21 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
         user: req.user.id
     });
     newPost.save().then(post => res.json(post))
+})
+
+//Delete route: api/post/:id 
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req,res) => {
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            Post.findById(req.params.id)
+            .then(post => {
+                if(post.user.toString() !== req.user.id){
+                    return res.status(401).json({notauthorized:'User not authorized'})
+                }
+                post.remove().then(() => res.json({success: true}))
+            })
+        })
+        .catch(err => res.status(404).json({postnotfound: 'No post found'}))
 })
 
 module.exports = router;
