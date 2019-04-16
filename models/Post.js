27ddb3a1736c7389a9a03e5deb20875e1b47d846
sguittap <sguittap@gmail.com>
2019-04-16 13:39:06 -0400

    added post like and unlike

--- models/Post.js
+++ models/Post.js
@@ -7,7 +7,7 @@ const PostSchema = new Schema({
     text:{type: String, required: true},
     name:{type: String},
     avatar:{type: String},
-    like:[{user:{type: Schema.Types.ObjectId, ref: 'users'}}],
+    likes:[{user:{type: Schema.Types.ObjectId, ref: 'users'}}],
     comments:[{user:{type: Schema.Types.ObjectId, ref: 'users'},
         text:{type: String, required: true},
         name:{type: String},
