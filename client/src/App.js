76b575f4ec9e225fb8e79049e3825b5427b9e5c5
sguittap <sguittap@gmail.com>
2019-04-17 17:05:02 -0400

    added basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Layout/Landing';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div >
-        <h1>My React app</h1>
+        <Navbar/>
+        <Landing/>
+        <Footer/>
       </div>
     );
   }
