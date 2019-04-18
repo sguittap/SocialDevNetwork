9227844c715590283665154b5be517b5d869859a
sguittap <sguittap@gmail.com>
2019-04-18 11:38:50 -0400

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import Navbar from './components/Layout/Navbar';
-import Footer from './components/Layout/Footer';
-import Landing from './components/Layout/Landing';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div >
-        <Navbar/>
-        <Landing/>
-        <Footer/>
-      </div>
+      <Router>
+        <div>
+          <Navbar/>
+          <Route exact path='/' component={Landing}/>
+          <div className='container'>
+            <Route exact path='/register' component={Register}/>
+            <Route exact path='/login' component={Login}/>
+          </div>
+          <Footer/>
+        </div>
+      </Router>
     );
   }
 }
