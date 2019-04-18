32d3c55bcaef44fb5eac70bc07a363aab074cabe
sguittap <sguittap@gmail.com>
2019-04-18 17:46:49 -0400

    added login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import {setCurrentUser} from './actions/authActions';
+
 import store from './store';
 
 import Navbar from './components/layout/Navbar';
@@ -10,6 +14,12 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
+
+if(localStorage.jwtToken){
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+};
 
 
 class App extends Component {
