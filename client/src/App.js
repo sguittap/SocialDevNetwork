f47fd42136925bad7f459b0a1c354259f83389f1
sguittap <sguittap@gmail.com>
2019-04-23 17:39:31 -0400

    created profile component and route

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 import PrivateRoute from './components/common/PrivateRoute';
 
 import './App.css';
@@ -47,6 +48,9 @@ class App extends Component {
               <Route exact path='/login' component={Login}/>
               <Switch>
                 <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
               </Switch>
             </div>
             <Footer/>
