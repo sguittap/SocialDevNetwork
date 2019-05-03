57aa3f17220255cef661443e99f4c1808a35fb4e
sguittap <sguittap@gmail.com>
2019-05-03 13:30:14 -0400

    redoing all of client

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,14 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import {setCurrentUser, logoutUser} from './actions/authActions';
-import {clearCurrentProfile} from './actions/profileActions';
+import React, {Fragment} from 'react';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
 
-
-import store from './store';
-
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/create-profile/CreateProfile';
-import PrivateRoute from './components/common/PrivateRoute';
-import EditProfile from './components/editProfile/editProfile';
-import AddExperience from './components/common/add-credentials/AddExperience';
 
 import './App.css';
 
-//Check for token -> set auth token as header -> decode token and get user info -> set user and isAuthenticated ->
-//logout user -> redirect to login
-if(localStorage.jwtToken){
-  setAuthToken(localStorage.jwtToken);
-  const decoded = jwt_decode(localStorage.jwtToken);
-  store.dispatch(setCurrentUser(decoded));
-  const currentTime = Date.now() / 1000;
-  if(decoded.exp <  currentTime){
-    store.dispatch(logoutUser());
-    store.dispatch(clearCurrentProfile());
-    window.location.href = '/login';
-  };
-};
-
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <div>
-            <Navbar/>
-            <Route exact path='/' component={Landing}/>
-            <div className='container'>
-              <Route exact path='/register' component={Register}/>
-              <Route exact path='/login' component={Login}/>
-              <Switch>
-                <PrivateRoute exact path='/dashboard' component={Dashboard}/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path='/add-experience' component={AddExperience}/>
-              </Switch>
-            </div>
-            <Footer/>
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
-}
+const App = () => 
+  <Fragment>
+    <Navbar/>
+    <Landing/>
+  </Fragment>
 
 export default App;
