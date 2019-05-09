fda131ef527a41b26e3ad33d9322676bd844cf4f
sguittap <sguittap@gmail.com>
2019-05-09 14:12:56 -0400

    adding private routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import {Provider} from 'react-redux';
 import Alert from './components/layout/Alert';
 import setAuthToken from './utils/setAuthToken';
 import {loadUser} from './actions/auth';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRouting';
 
 import './App.css';
 
@@ -33,6 +35,7 @@ const App = () => {
           <Switch>
             <Route exact path='/register' component={Register}/>
             <Route exact path='/login' component={Login}/>
+            <PrivateRoute exact path='/dashboard' component={Dashboard}/>
           </Switch>
         </section>
       </Fragment>
