5147d34e33733a10637364822c912216051d694d
sguittap <sguittap@gmail.com>
2019-05-09 17:31:53 -0400

    adding create-profile

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import setAuthToken from './utils/setAuthToken';
 import {loadUser} from './actions/auth';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRouting';
+import CreateProfile from './components/profile-form/CreateProfile';
 
 import './App.css';
 
@@ -36,6 +37,7 @@ const App = () => {
             <Route exact path='/register' component={Register}/>
             <Route exact path='/login' component={Login}/>
             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+            <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
           </Switch>
         </section>
       </Fragment>
