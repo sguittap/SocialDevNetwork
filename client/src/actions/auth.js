6a110cbee960707b27ef3a1e25d93acfa377d091
sguittap <sguittap@gmail.com>
2019-05-09 16:31:27 -0400

    adding to dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT} from './types';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_PROFILE} from './types';
 import {setAlert} from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -58,5 +58,7 @@ export const login = (email, password) =>  async dispatch => {
 
 //Logout
 export const logout = () => dispatch => {
+    dispatch({type: CLEAR_PROFILE});
     dispatch({type: LOGOUT});
+    
 };
--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -10,17 +10,23 @@ const Navbar = ({auth: {isAuthenticated, loading}, logout}) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to="/" onClick={logout}>
-        <i className='fas fa-sign-out-alt'></i>
-        {' '}<span className='hide-sm'>Logout</span> 
+        <Link to="/dashboard">
+          <i className='fas fa-user'/>{' '}
+          <span className='hide-sm'>Dashboard</span>
         </Link>
+      </li>
+      <li>
+        <a onClick={logout} href='#!'>
+        <i className='fas fa-sign-out-alt'/>{' '}
+        <span className='hide-sm'>Logout</span> 
+        </a>
       </li>
     </ul>
   );
 
   const guestLinks = (
     <ul>
-      <li><Link to="/">Developers</Link></li>
+      <li><a href="#!">Developers</a></li>
       <li><Link to="/register">Register</Link></li>
       <li><Link to="/login">Login</Link></li>
     </ul>
