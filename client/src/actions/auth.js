1a6ffee84ea0356c5e82818674a016f8e897f9b8
sguittap <sguittap@gmail.com>
2019-05-09 13:35:36 -0400

    logout and navbar links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL} from './types';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT} from './types';
 import {setAlert} from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -54,4 +54,9 @@ export const login = (email, password) =>  async dispatch => {
         }
         dispatch({type: LOGIN_FAIL})
     };
+};
+
+//Logout
+export const logout = () => dispatch => {
+    dispatch({type: LOGOUT});
 };
