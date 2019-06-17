0a212bdf9d36042a5002bcced90fb6673a379783
sguittap <sguittap@gmail.com>
2019-06-17 11:14:47 -0400

    added profile actions and reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE} from './types';
+import {GET_PROFILE,GET_PROFILES, PROFILE_ERROR, UPDATE_PROFILE, ACCOUNT_DELETED, CLEAR_PROFILE, GET_REPOS} from './types';
 import {setAlert} from './alert';
 
 export const getCurrentProfile = () => async dispatch => {
@@ -10,6 +10,38 @@ export const getCurrentProfile = () => async dispatch => {
      dispatch({type: PROFILE_ERROR, payload:{msg: err.response.statusText, status: err.response.status}})   
     };
 };
+
+//get all profile
+export const getProfiles = () => async dispatch => {
+    dispatch({type: CLEAR_PROFILE})
+    try {
+        const res = await axios.get('/api/profile');
+        dispatch({type: GET_PROFILES, payload: res.data})
+    } catch (err) {
+     dispatch({type: PROFILE_ERROR, payload:{msg: err.response.statusText, status: err.response.status}})   
+    };
+};
+
+//get profile by id
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+        dispatch({type: GET_PROFILE, payload: res.data})
+    } catch (err) {
+     dispatch({type: PROFILE_ERROR, payload:{msg: err.response.statusText, status: err.response.status}})   
+    };
+};
+
+//get github repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+        dispatch({type: GET_REPOS, payload: res.data})
+    } catch (err) {
+     dispatch({type: PROFILE_ERROR, payload:{msg: err.response.statusText, status: err.response.status}})   
+    };
+};
+
 
 //create or update profile
 export const createProfile = (formData, history, edit = false) => async dispatch => {
