50999b2f4a995e55083db9afad135b981e8e0011
sguittap <sguittap@gmail.com>
2019-06-21 19:21:40 -0400

    fixing typos

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ if(process.env.NODE_ENV === 'produciton'){
     });
 }
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
