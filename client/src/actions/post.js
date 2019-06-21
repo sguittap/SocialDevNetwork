26aea6d8e099a0688d7b071a85b6ccf7e6cddbb8
sguittap <sguittap@gmail.com>
2019-06-21 12:55:11 -0400

    added like and unlike functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {setAlert} from './alert';
-import {GET_POSTS, POST_ERROR} from './types';
+import {GET_POSTS, POST_ERROR, UPDATE_LIKES} from './types';
 
 
 //get posts
@@ -8,6 +8,26 @@ export const getPosts = () => async dispatch => {
     try {
         const res = await axios.get('/api/posts');
         dispatch({type: GET_POSTS, payload: res.data})
+    } catch (err) {
+        dispatch({type: POST_ERROR, payload:{msg: err.response.statusText, status: err.response.status}})   
+    };
+};
+
+//add posts
+export const addLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${id}`);
+        dispatch({type: UPDATE_LIKES, payload: {id, likes: res.data}})
+    } catch (err) {
+        dispatch({type: POST_ERROR, payload:{msg: err.response.statusText, status: err.response.status}})   
+    };
+};
+
+//remove likes
+export const removeLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${id}`);
+        dispatch({type: UPDATE_LIKES, payload: {id, likes: res.data}})
     } catch (err) {
         dispatch({type: POST_ERROR, payload:{msg: err.response.statusText, status: err.response.status}})   
     };
