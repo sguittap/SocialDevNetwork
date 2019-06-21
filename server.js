aae4736a3fa62ddc35d2b9cba195db740541a123
sguittap <sguittap@gmail.com>
2019-06-21 16:33:48 -0400

    preparing to deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 const auth = require('./routes/api/auth');
+const path = require('path');   
 
 require('dotenv').config();
 
@@ -29,6 +30,14 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 app.use('/api/auth', auth);
+
+//Serve static assests in production
+if(process.env.NODE.ENV === 'produciton'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
